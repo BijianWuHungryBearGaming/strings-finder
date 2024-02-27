16d33c902828e819b49b7c11c133377abc7848f4
BijianWuHungryBearGaming <106966132+BijianWuHungryBearGaming@users.noreply.github.com>
2024-02-27 17:01:11 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Divider, Grid } from "@mui/material";
 import SearchForm from "./components/SearchForm";
 
 function App() {
@@ -7,22 +7,27 @@ function App() {
     <Grid container rowSpacing={5} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         <Grid item xs={12}>
             <SearchForm label={"Reel set 1"}/>
+            <Divider>DIVIDER</Divider>
         </Grid>
 
         <Grid item xs={12} >
             <SearchForm label={"Reel set 2"}/>
+            <Divider>DIVIDER</Divider>
         </Grid>
 
         <Grid item xs={12} >
             <SearchForm label={"Reel set 3"}/>
+            <Divider>DIVIDER</Divider>
         </Grid>
 
         <Grid item xs={12} >
             <SearchForm label={"Reel set 4"}/>
+            <Divider>DIVIDER</Divider>
         </Grid>
 
         <Grid item xs={12} >
             <SearchForm label={"Reel set 5"}/>
+            <Divider>DIVIDER</Divider>
         </Grid>
 
 
